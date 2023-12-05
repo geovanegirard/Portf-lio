56844371dc88f5f3ae174fce0f309657626afd96
Geovane Girard <g.ng2009@hotmail.com>
2023-12-05 17:30:18 -0300

    Validando o formulário

--- estatic/script.js
+++ estatic/script.js
@@ -18,3 +18,24 @@ navItem.forEach(item => {
         }
     })
 })
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'service_dwjex4g';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
